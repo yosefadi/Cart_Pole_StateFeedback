a524f2ab18d22b23b04dff75a8e8836ee9ece990
Yosef Adi Sulistyo <10355397+yosefadi@users.noreply.github.com>
2022-11-26 11:40:37 +0700

    Update MATLAB script to match matrix dim

--- MATLAB_Computation/statespace_cartpole.m
+++ MATLAB_Computation/statespace_cartpole.m
@@ -8,7 +8,7 @@ mp = 0.1;
 l = 0.5;
 
 A = [0 1 0 0; 0 0 -mp*(mp * (g-l) + mc*g)/((mc+mp)*((4/3) * mc + (1/3) * mp)) 0; 0 0 0 1; 0 0 (mp*(g-l) + mc * g)/(l*((4/3) * mc + (1/3) * mp)) 0]
-B = [0 (1/(mc + mp) - mp/((mc + mp) * ((4/3) * mc + (1/3) * mp))) 0 (-1/(l * ((4/3) * mc + (1/3) * mp)))]
+B = transpose([0 (1/(mc + mp) - mp/((mc + mp) * ((4/3) * mc + (1/3) * mp))) 0 (-1/(l * ((4/3) * mc + (1/3) * mp)))])
 C = [1 0 0 0; 0 0 1 0]
 At = transpose(A);
 Bt = transpose(C);
