580af0a1573ad00d51adec9d9e256266e0b01f19
Yosef Adi Sulistyo <10355397+yosefadi@users.noreply.github.com>
2022-11-26 13:53:15 +0700

    Finally chall 1 done

--- 1_StateCompensator.py
+++ 1_StateCompensator.py
@@ -28,34 +28,38 @@ B = np.array([[0],
 C = np.array([[1, 0, 0, 0],
               [0, 0, 1, 0]])
 
-L = 10**6 * np.array([[0.0032, 0.0022],
-                      [2.0835, 1.8055],
-                      [-0.0010, 0.0006],
-                      [-1.2469, -0.4731]])
-# place the regulator pole to -1, -1+j, -1-j, -2
-K = 10**8 * np.array([[-1.6618,-0.0277,-0.9583,-0.0160]])
+# place estimator pole to -60,-0.5+i,-0.5-i,-120
+L = 10**0 * np.array([[60.0004, -2.0107],
+                      [0.0336, -121.7227],
+                      [-0.0095, 120.9996],
+                      [74.3410, 136.9118]])
+# place the regulator pole to -10, -0.5+i, -0.5-i, -20
+K = 10**0 * np.array([[-20.5155,-19.4897,-180.5628,-32.4047]])
 
 dt = 0.02
 
 def compute_state_estimator(A, B, C, L, x_hat, y, u, dt):
-    x_hat_dot = A@x_hat + B@u + L@(y-C@x_hat)
+    x_hat_dot = A@x_hat + B@u + L@(y - C@x_hat)
     return x_hat_dot
 
 def apply_state_controller(K, x):
     # feedback controller
     # MODIFY THIS PARTS
+    #print(x)
     u = -K@x   # u = -Kx
     #print(u)
     return u
 
 obs_hat = obs
-print(obs)
+#print(obs)
+iter = 0
 for i in range(1000):
     env.render()
 
     # get force direction (action) and force value (force)
 
     # MODIFY THIS PART
+    
     force = apply_state_controller(K, obs_hat)
     if force > 0:
         action = 1
@@ -77,7 +81,9 @@ for i in range(1000):
     obs_hat = obs_hat + obs_hat_dot*dt
     y_hat = C@obs_hat
     error = obs - obs_hat
-    #print(error)
+    print(error)
+
+    iter = iter+1
 
     reward_total = reward_total+reward
     if done or truncated:
