f6769abfeb860fc1f2097f4c9bb3548885ee9bbf
Yosef Adi Sulistyo <10355397+yosefadi@users.noreply.github.com>
2022-11-27 18:55:23 +0700

    Add some information on done for robust tracking

--- 3_RobustTracking.py
+++ 3_RobustTracking.py
@@ -78,13 +78,18 @@ obs_aug = np.block([[np.zeros([C.shape[0],1])],
 obs_aug = np.reshape(obs_aug, obs_aug.shape[0])
 force = np.zeros([1,])
 
+u_array = []
+x_array = []
+theta_array = []
+
 for i in range(1000):
     env.render()
     
     # MODIFY THIS PART
     action, force = apply_state_controller(obs_aug)
-
+    u_array.append(force)
     force = force + w
+
     # absolute value, since 'action' determines the sign, F_min = -10N, F_max = 10N
     abs_force = abs(float(np.clip(force, -10, 10)))
     
@@ -93,6 +98,8 @@ for i in range(1000):
 
     # apply action
     obs, reward, done, truncated, info = env.step(action)
+    x_array.append(obs[0])
+    theta_array.append(obs[2])
 
     obs_aug_dot = f_aug_linear(obs_aug, force)
     obs_aug = obs_aug + obs_aug_dot * dt
@@ -104,11 +111,43 @@ for i in range(1000):
         obs_aug[n+C.shape[0]] = obs[n]
 
     reward_total = reward_total+reward
+    print()
     if done or truncated:
-        ess = obs[0] - 0
-        print("ess: ", ess)
         print(f'Terminated after {i+1} iterations.')
         print(reward_total)
+
+        x_array_abs = []
+        for i in range(len(x_array)):
+            x_array_abs.append(abs(x_array[i]))
+        
+        ess_avg = np.around(np.mean(x_array_abs),3)
+        print("ess: ", ess_avg)
+
+        u_array_abs = []
+        for i in range(len(u_array)):
+            u_array_abs.append(abs(u_array[i]))
+        u_avg = np.around(np.mean(u_array_abs),3)
+        print("u_avg: ", u_avg, "N")
+
+        theta_min = np.amin(theta_array)
+        theta_max = np.amax(theta_array)
+        if theta_max > np.abs(theta_min):
+            theta_abs = theta_max
+            search_theta = theta_max
+        else:
+            theta_abs = np.abs(theta_min)
+            search_theta = theta_min
+        
+        overshoot_rad = np.around(theta_abs, 3)
+        overshoot_deg = np.around(np.rad2deg(theta_abs),3)
+        print("overshoot: ", overshoot_deg, "degree")
+
+        for i in range(len(theta_array)):
+            if theta_array[i] == search_theta:
+                peak_time = np.around(i * dt,3)
+                print("peak_time: ", peak_time, "s")
+                break
+
         obs, info = env.reset()
         break
 
