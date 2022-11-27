Fix peak_time computation algo

3_RobustTracking.py
@@ -45,7 +45,7 @@ w = np.reshape(w,1)
 
 # desired pole
 P = np.array([-0.25+0.5j, -0.25-0.5j, -10, -20])
-P_aug = np.array([-1+1j,-1-1j,-2+2j,-2-2j,-15])
+P_aug = np.array([-2+0.5j,-2-0.5j,-1.75+0.25j,-1.75-0.25j,-100])
 
 # compute regulator gain
 K = control.place(A,B,P)
@@ -156,7 +156,7 @@ for i in range(1000):
 
         for i in range(len(theta_array)):
             if np.abs(theta_array[i]) < 1e-3:
-                peak_time = np.around(i * dt,3)
+                peak_time = np.around(i * dt,3)/2
                 print("peak_time: ", peak_time, "s")
                 break
 